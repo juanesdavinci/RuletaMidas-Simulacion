a9e2efaf96dc9339b28328b33e7a0a404b332234
Juanesdavinci <juanesdavinci@gmail.com>
2020-02-15 08:30:23 -0500

    primer commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class Ball : MonoBehaviour
+{
+
+    [Range(0, 100000)] public float InitialSpeed;
+    public Rigidbody rb;
+    public float angle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.isKinematic = true;
+        StartCoroutine(startForce());
+
+    }
+
+    private IEnumerator startForce()
+    {
+        yield return new WaitForSeconds(1);
+        rb.isKinematic = false;
+        GetComponent<Rigidbody>().AddRelativeForce(transform.right * InitialSpeed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position,transform.right);
+    }
+
+    private void FixedUpdate3()
+    {
+        angle = Mathf.Atan(transform.position.x / transform.position.y);
+        angle += Mathf.PI / 2;
+        GetComponent<Rigidbody>().AddRelativeForce(transform.right * InitialSpeed);
+    }
+}
---
+++ Assets/Rotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotor : MonoBehaviour
+{
+
+    [Range(0, 100)] public float rpm;
+
+    public Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hertz = 1 / rpm;
+
+        float angles = 360 * Time.deltaTime * hertz;
+
+        transform.Rotate(rotation * angles);
+    }
+}
